01f0b077a90daa8f48771d9cec13afc03bbc5d28
21WANGC <wangc21@uw.edu>
2018-10-13 14:29:10 -0700

    added alice in wonderland

--- lstm_rnn.py
+++ lstm_rnn.py
@@ -4,4 +4,17 @@ from keras.layers import Dense, Dropout, LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 
-book = open('')
+#load data
+file_path = 'data\\alice_in_wonderland.txt'
+book = open(file_path, encoding="utf8").read().lower()
+
+#covert unique chars to int
+chars = sorted(list(set(book)))
+char_int_dict = {}
+for integer, character in enumerate(chars):
+	char_int_dict[character] = integer
+
+total_chars = len(book)
+unique_chars = len(chars)
+print(total_chars)
+print(unique_chars)
