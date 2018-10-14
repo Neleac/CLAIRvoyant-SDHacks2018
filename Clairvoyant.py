586ca4ad949c062b23c308b18e7e9830bd74656d
21WANGC <wangc21@uw.edu>
2018-10-13 18:40:58 -0700

    working frontend with error handling

--- Clairvoyant.py
+++ Clairvoyant.py
@@ -1,29 +1,72 @@
-#!pip install --upgrade pip
-#!pip install clarifai --upgrade
-
-import clarifai
 from clarifai.rest import ClarifaiApp
-from clarifai.rest import Image as ClImage
+import cv2
 
-MY_API_KEY = '9f14ea7b98e0449d8f65509d5b882896'
+MY_API_KEY = 'e77c49d9b41845bb82caf94ab3f1471f'
 app = ClarifaiApp(api_key = MY_API_KEY)
 
-model = app.models.get('general-v1.3')
-#image = ClImage(file_obj=open('/Users/depressedonion/Downloads/kittenSmol.jpg', 'rb'))
-print("Type location: ")
-imageLocation = input()
-if imageLocation.startswith('https'):
-    image = ClImage(url=imageLocation)
-response = model.predict([image])
+model = app.public_models.general_model
 
-outputs = response['outputs']
-stuff = outputs[0]
-infoEach = stuff['data']
-concepts = infoEach['concepts']
+def take_image():
+	cam = cv2.VideoCapture(0)
 
-listOfWords = []
-for one in concepts:
-    listOfWords.append(one['name'])
+	while True:
+		ret, frame = cam.read()
+		cv2.imshow('frame', frame)
+		
+		key = cv2.waitKey(1)
 
-for word in listOfWords:
-    print(word)
+		if key%256 == 27:
+			# ESC pressed
+			print("Escape hit, closing...")
+			break
+		elif key%256 == 32:
+			# SPACE pressed
+			cv2.imwrite('images\\opencv_frame.jpg', frame)
+			print("Image taken")
+			break
+
+	cam.release()
+	cv2.destroyAllWindows()
+
+def predict(address):
+	response = model.predict_by_filename(address)
+
+	concepts = response['outputs'][0]['data']['concepts']
+
+	concept_list = []
+	for concept in concepts:
+		concept_list.append(concept['name'])
+		if len(concept_list) == 10:
+			break
+
+	for concept in concept_list:
+		print(concept)
+
+
+print("Hi there, my name is Clair. I am an AI image recognition agent based on Clarifai's vision API. Give me an image, and I'll give you my interpretation.\n")
+
+#get image location
+valid_response = False
+while not valid_response:
+	print("Enter 1 to take a picture from webcam\n")
+	print("Enter 2 to choose a local image\n")
+	print("Enter Q to quit")
+	response = input()
+	if response == str(1):
+		valid_response = True
+		print("Press Space to take picture")
+		take_image()
+		predict('images\\opencv_frame.jpg')
+	elif response == str(2):
+		print("Enter image location")
+		address = input()
+		try:
+			predict(address)
+			valid_response = True
+		except:
+			print("Invalid address\n")
+	elif response == "Q":
+		print("Goodbye!")
+		break
+	else:
+		print("Invalid input\n")
--- lstm_rnn.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
-from keras.callbacks import ModelCheckpoint
-from keras.utils import np_utils
-
-book = open('')
